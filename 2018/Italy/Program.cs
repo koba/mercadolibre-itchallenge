9854d9de55466a7e22e86ce8c14aa9bc491d8f1d
Gastón Donadío <tongadonadio@gmail.com>
2018-09-23 15:04:33 -0300

    Solución de italia

---
+++ 2018/Italy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoLibre.Italy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = System.IO.File.ReadAllText(@"C:\Users\Tonga\Documents\Visual Studio 2015\Projects\mercadolibre.USA\inputItaly.txt");
+
+            int p = 0;
+            int c = 0;
+            int m = 0;
+            int b = 0;
+            int z = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'p')
+                    p++;
+                if (line[i] == 'c')
+                    c++;
+                if (line[i] == 'm')
+                    m++;
+                if (line[i] == 'b')
+                    b++;
+                if (line[i] == 'z')
+                    z++;
+
+            }
+
+            Console.WriteLine(Math.Min(Math.Min(Math.Min(Math.Min(p, c), m), b), z));
+            Console.Read();
+        }
+    }
+}
